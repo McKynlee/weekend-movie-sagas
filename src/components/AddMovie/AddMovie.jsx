748e3f407b59814ce0b0876e72c4b0ca2ece07a8
McKynlee Westman <mckynlee.westman@gmail.com>
2021-03-05 19:52:28 -0600

    Add Movie title input created and capturing input values

--- src/components/AddMovie/AddMovie.jsx
+++ src/components/AddMovie/AddMovie.jsx
@@ -1,6 +1,25 @@
+import { useDispatch } from "react-redux";
+import { useState } from 'react';
+
+// Form page for users to add movie to the list:
 function AddMovie() {
+  const dispatch = useDispatch;
+
+  // Create local state to store inputs as user types:
+  const [title, setTitle] = useState('');
+  const [posterURL, setPosterURL] = useState('');
+  const [description, setDescription] = useState('');
+  const [genre, setGenre] = useState('');
+
+  console.log('title:', title);
+
   return (
-    <div>AddMovie test</div>
+    <div>
+      <input type="text" value={title}
+        placeholder="Movie Title"
+        onChange={(event) => { setTitle(event.target.value) }}
+      />
+    </div>
   )
 } //end AddMovie
 
