70e6967810b5d2ef3ef9338353c4a95869f9ce67
McKynlee Westman <mckynlee.westman@gmail.com>
2021-03-05 19:29:19 -0600

    created AddMovie and Details components, importing into App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,18 @@
-import {HashRouter as Router, Route} from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import './App.css';
-import MovieList from '../MovieList/MovieList'
+import MovieList from '../MovieList/MovieList';
+import MovieItemDetails from '../MovieItemDetails/MovieItemDetails';
+import AddMovie from '../AddMovie/AddMovie';
 
 function App() {
   return (
     <div className="App">
       <h1>The Movies Saga!</h1>
-      <Router>        
+      <Router>
         <Route path="/" exact>
           <MovieList />
         </Route>
-        
+
         {/* Details page */}
 
         {/* Add Movie page */}
