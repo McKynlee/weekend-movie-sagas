498fa0a9575a80deecba091dca6e71df6658e45d
McKynlee Westman <mckynlee.westman@gmail.com>
2021-03-06 14:34:26 -0600

    Back to List button functional

--- src/components/MovieItemDetails/MovieItemDetails.jsx
+++ src/components/MovieItemDetails/MovieItemDetails.jsx
@@ -1,19 +1,25 @@
 import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
 function MovieItemDetails() {
-  const dispatch = useDispatch();
+  const history = useHistory();
 
   // Bring movieDetails in from Reducer
   // in order to render them on page:
   const movieDetails = useSelector(store =>
     store.movieDetails)
-
   console.log('movieDetails:', movieDetails);
+
+  // Navigate back to movie list on 'Back to List' click:
+  const backToList = () => {
+    history.push('/');
+  }
 
   return (
     <div>
-      TEST movie item details
+      <div>{movieDetails[0].description}</div>
+      <button onClick={backToList}>Back to List</button>
     </div>
   )
 } // end MovieItemDetails
