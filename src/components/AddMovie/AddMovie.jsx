15a7b37d807fd281367fc9b52a5ba7f69a522053
McKynlee Westman <mckynlee.westman@gmail.com>
2021-03-06 08:57:07 -0600

    Dispatching inputs to saga via payload

--- src/components/AddMovie/AddMovie.jsx
+++ src/components/AddMovie/AddMovie.jsx
@@ -1,10 +1,10 @@
 import { useDispatch } from "react-redux";
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { useHistory } from "react-router-dom";
 
 // Form page for users to add movie to the list:
 function AddMovie() {
-  const dispatch = useDispatch;
+  const dispatch = useDispatch();
   const history = useHistory();
 
   // Create local state to store inputs as user types:
@@ -13,48 +13,71 @@ function AddMovie() {
   const [description, setDescription] = useState('');
   const [genre, setGenre] = useState('');
 
+  // Test that inputs are captured:
   console.log('title:', title,
     'posterUrl:', posterURL,
     'description:', description,
     'genre:', genre);
 
+  // On click of cancel button, navigate to home
   const handleCancel = () => {
     console.log('handleCancel');
     history.push('/');
   }
 
+  // When inputs submitted, dispatch to saga
+  // to then be sent to db/reducer
+  const saveNewMovie = (event) => {
+    event.preventDefault();
+    dispatch({
+      type: 'CREATE_MOVIE',
+      payload: {
+        title,
+        poster: posterURL,
+        description,
+        genre
+      }
+    })
+  } // end saveNewMovie
+
   return (
     <div>
-      <label>Title:
+      <form onSubmit={saveNewMovie}>
+        <label>Title:
       <input type="text" value={title}
-          placeholder="Movie Title"
-          onChange={event => setTitle(event.target.value)}
-        />
-      </label>
-      <label>Movie Poster URL:
+            placeholder="Movie Title"
+            onChange={event =>
+              setTitle(event.target.value)}
+          />
+        </label>
+        <label>Movie Poster URL:
       <input type="text" value={posterURL}
-          placeholder="Poster URL"
-          onChange={event => setPosterURL(event.target.value)}
-        />
-      </label>
-      <label>Description:
+            placeholder="Poster URL"
+            onChange={event =>
+              setPosterURL(event.target.value)}
+          />
+        </label>
+        <label>Description:
         <textarea id="description"
-          name="description" rows="4" cols="10"
-          value={description}
-          onChange={event => setDescription(event.target.value)}
-        />
-      </label>
-      <select onChange={event => setGenre(event.target.value)}>
-        <option value=''>Choose a Genre</option>
-        <option>Test Genre</option>
-        {/* .map genre list here */}
-      </select>
-      <button onClick={handleCancel}>
-        Cancel
+            name="description" rows="4" cols="10"
+            value={description}
+            onChange={event =>
+              setDescription(event.target.value)}
+          />
+        </label>
+        <select onChange={event =>
+          setGenre(event.target.value)}>
+          <option value=''>Choose a Genre</option>
+          <option>Test Genre</option>
+          {/* .map genre list here */}
+        </select>
+        <button onClick={handleCancel}>
+          Cancel
         </button>
-      <button>
-        Save
+        <button>
+          Save
         </button>
+      </form>
     </div>
   )
 } //end AddMovie
--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App.js';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import {
+  createStore,
+  combineReducers,
+  applyMiddleware
+} from 'redux';
 // Provider allows us to use redux within our react app
 import { Provider } from 'react-redux';
 import logger from 'redux-logger';
@@ -13,63 +17,69 @@ import axios from 'axios';
 
 // Create the rootSaga generator function
 function* rootSaga() {
-    yield takeEvery('FETCH_MOVIES', fetchAllMovies);
+  yield takeEvery('FETCH_MOVIES', fetchAllMovies);
+  yield takeEvery('CREATE_MOVIE', createMovie);
 }
 
 function* fetchAllMovies() {
-    // get all movies from the DB
-    try {
-        const movies = yield axios.get('/api/movie');
-        console.log('get all:', movies.data);
-        yield put({ type: 'SET_MOVIES', payload: movies.data });
+  // get all movies from the DB
+  try {
+    const movies = yield axios.get('/api/movie');
+    console.log('get all:', movies.data);
+    yield put({ type: 'SET_MOVIES', payload: movies.data });
 
-    } catch {
-        console.log('get all error');
-    }
-        
+  } catch {
+    console.log('get all error');
+  }
+
 }
 
-// Create sagaMiddleware
+function* createMovie(action) {
+  // add input movie from AddMovie.jsx to the DB
+  console.log('createMovie action:', action);
+}
+// Create sagaMiddleware so that we 
+// can activate sagas in the storeInstance
 const sagaMiddleware = createSagaMiddleware();
 
 // Used to store movies returned from the server
 const movies = (state = [], action) => {
-    switch (action.type) {
-        case 'SET_MOVIES':
-            return action.payload;
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case 'SET_MOVIES':
+      return action.payload;
+    default:
+      return state;
+  }
 }
 
 // Used to store the movie genres
 const genres = (state = [], action) => {
-    switch (action.type) {
-        case 'SET_GENRES':
-            return action.payload;
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case 'SET_GENRES':
+      return action.payload;
+    default:
+      return state;
+  }
 }
 
 // Create one store that all components can use
 const storeInstance = createStore(
-    combineReducers({
-        movies,
-        genres,
-    }),
-    // Add sagaMiddleware to our store
-    applyMiddleware(sagaMiddleware, logger),
+  combineReducers({
+    movies,
+    genres,
+  }),
+  // Add sagaMiddleware to our store
+  applyMiddleware(sagaMiddleware, logger),
 );
 
 // Pass rootSaga into our sagaMiddleware
 sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
-    <React.StrictMode>
-        <Provider store={storeInstance}>
-        <App />
-        </Provider>
-    </React.StrictMode>,
-    document.getElementById('root')
+  <React.StrictMode>
+    <Provider store={storeInstance}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
