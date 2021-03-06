840b062475d571c1b96fe747bce6d61731e6f62f
McKynlee Westman <mckynlee.westman@gmail.com>
2021-03-06 14:57:08 -0600

    Rendering movie details onto page including ALL associated genres

--- src/components/MovieItemDetails/MovieItemDetails.jsx
+++ src/components/MovieItemDetails/MovieItemDetails.jsx
@@ -1,15 +1,23 @@
-import { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
+import { useEffect } from 'react';
 
 function MovieItemDetails() {
   const history = useHistory();
+  const dispatch = useDispatch();
+
+  // bring in selected movies on page load:
+  // useEffect(() => {
+  //   dispatch({ type: 'FETCH_MOVIE_DETAILS' });
+  // }, []);
 
   // Bring movieDetails in from Reducer
   // in order to render them on page:
-  const movieDetails = useSelector(store =>
-    store.movieDetails)
+  const movieDetails = useSelector(store => store.movieDetails)
   console.log('movieDetails:', movieDetails);
+
+  // declare variable to represent genre array within movieDetails:
+  const genreArray = movieDetails[0].genres;
 
   // Navigate back to movie list on 'Back to List' click:
   const backToList = () => {
@@ -17,10 +25,23 @@ function MovieItemDetails() {
   }
 
   return (
-    <div>
-      <div>{movieDetails[0].description}</div>
+    <main>
+      <section>
+        <h3>{movieDetails[0].title}</h3>
+        <img src={movieDetails[0].poster} alt={movieDetails[0].title} />
+      </section>
+      <section>
+        <h4>Associate Genres:</h4>
+        {genreArray.map(genre => {
+          return (
+            <div>{genre}</div>
+          )
+        })}
+        <div>{movieDetails[0].description}</div>
+      </section>
+
       <button onClick={backToList}>Back to List</button>
-    </div>
+    </main>
   )
 } // end MovieItemDetails
 
